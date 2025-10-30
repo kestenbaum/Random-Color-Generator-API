3b7bd66c8347777fe6ee00a918a19ba8f4faf013
Oleksii <kespa71for@gmail.com>
2025-10-30 14:50:12 +0100

    feat(app): add generator color in json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { randomInt, rgbToHsl } from "./utils";
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
-app.get("/", (req, res: express.Response): void => {
-    res.send("Hello World!");
-})
+app.get("/color", (req, res) => {
+    const r = randomInt(0, 255);
+    const g = randomInt(0, 255);
+    const b = randomInt(0, 255);
 
-app.listen(port, "0.0.0.0", () => console.log(`Listening on ${port}`));
+    const hex = '#' + [r, g, b]
+            .map(v => v.toString(16).padStart(2, '0'))
+            .join('');
+    const rgb = `rgb(${r}, ${g}, ${b})`;
+    const hsl = rgbToHsl(r, g, b);
+
+    res.json({ hex, rgb, hsl });
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Color API running on port ${port}`);
+});
