b1cfd8305d55902425dda3c9adf91e24dde81c34
Oleksii <kespa71for@gmail.com>
2025-11-02 13:47:03 +0100

    refactor(app): refactor code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ const hslButton = document.getElementById("hsl");
 async function getRandomColor() {
 	const res = await fetch(api);
 	if (!res.ok) throw new Error(res.statusText);
-			
+
 	return res.json();
 }
 
@@ -18,9 +18,9 @@ async function updateColor(format) {
 		let color = data.hex;
 		if (format === "rgb") color = data.rgb;
 		if (format === "hsl") color = data.hsl;
-				
+
 		document.body.style.backgroundColor  = color;
-				
+
 		hexButton.onclick = () => updateColor(color);
 		hslButton.onclick = () => updateColor(color);
 		rgbButton.onclick = () => updateColor(color);
@@ -28,4 +28,5 @@ async function updateColor(format) {
 		console.error(err);
 	}
 }
-updateColor("hex");
+
+await updateColor("hex");
