5b29a1bd4ae220f370b335226f8a863d91d6ee77
Oleksii <kespa71for@gmail.com>
2025-11-02 13:35:37 +0100

    refactor(app): refactor code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,27 +5,27 @@ const rgbButton = document.getElementById("rgb");
 const hslButton = document.getElementById("hsl");
 
 async function getRandomColor() {
-			const res = await fetch(api);
-			if (!res.ok) throw new Error(res.statusText);
+	const res = await fetch(api);
+	if (!res.ok) throw new Error(res.statusText);
 			
-			return res.json();
+	return res.json();
 }
 
 async function updateColor(format) {
 		try {
-				const data = await getRandomColor();
+			const data = await getRandomColor();
+
+			let color = data.hex;
+			if (format === "rgb") color = data.rgb;
+			if (format === "hsl") color = data.hsl;
 				
-				let color = data.hex;
-				if (format === "rgb") color = data.rgb;
-				if (format === "hsl") color = data.hsl;
+			document.body.style.backgroundColor  = color;
 				
-				document.body.style.backgroundColor  = color;
-				
-				hexButton.onclick = () => updateColor(color);
-				hslButton.onclick = () => updateColor(color);
-				rgbButton.onclick = () => updateColor(color);
+			hexButton.onclick = () => updateColor(color);
+			hslButton.onclick = () => updateColor(color);
+			rgbButton.onclick = () => updateColor(color);
 		} catch (err) {
-				console.error(err);
+			console.error(err);
 		}
 }
 updateColor("hex");
